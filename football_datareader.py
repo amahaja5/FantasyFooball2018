a103ece949aea4c47ce78bb55c3f517ec0a41290
Avi Mahajan <mahajan.avi@gmail.com>
2018-08-11 17:28:20 -0700

    Added year-by-year stats, also some basic error checking (prolly should have been there before).

--- football_datareader.py
+++ football_datareader.py
@@ -1,8 +1,9 @@
 from html.parser import HTMLParser
 import urllib.request
+import urllib.response
 import pandas as pd
 
-PFR = 'https://www.pro-football-reference.com/{}.htm'
+PFR = 'https://www.pro-football-reference.com/{}'
 
 class MyHTMLParser(HTMLParser):
     data_sources = []
@@ -16,11 +17,18 @@ class MyHTMLParser(HTMLParser):
             self.data_sources.append(data.strip())
             self.parse_data = False
 
-def pull_hist_stats_by_name(name):
+def pull_hist_stats(name, year=0):
     first_last = name.split()
+    
     plr_stng = "players/"+first_last[1][0]+"/"+first_last[1][0:4]+first_last[0][0:2]
+    
     num=find_player(name, plr_stng)
-    return pd.read_html(PFR.format(plr_stng+num), skiprows=1, header=0)[0]
+    if year > 0:
+        return pd.read_html(PFR.format(plr_stng+num+'/gamelog/'+str(year)), skiprows=[0], header=0)[0].iloc[0:-1,:].dropna(axis=1, how='all')
+    
+    return pd.read_html(PFR.format(plr_stng+num+'.htm'), skiprows=[0],
+                        header=0)[0].iloc[0:-1,:].dropna(axis=1,
+                                                         how='all')
 
 def find_player(name, plr_stng):
     plr_name = ""
@@ -29,7 +37,10 @@ def find_player(name, plr_stng):
         num_st = str(num)
         if num < 10:
             num_st = '0' + num_st
-        fp = urllib.request.urlopen(PFR.format(plr_stng + num_st))
+        fp = urllib.request.urlopen(
+            PFR.format(plr_stng + num_st + '.htm'))
+        if fp.geturl() == PFR.format('404.html?redir'):
+            raise NameError('Player Not Found')
         mybytes = fp.read()
         mystr = mybytes.decode("utf8")
         fp.close()
